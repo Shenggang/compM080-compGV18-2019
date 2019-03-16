4f68e20a0a65fce8c4810c80ce851273adee2139
Shenggang Hu <hushenggang1996@163.com>
2019-03-16 21:22:34 +0000

    mass matrix function completed

--- tutorial_004/cpp/practices_laplacian/src/mytools.cpp
+++ tutorial_004/cpp/practices_laplacian/src/mytools.cpp
@@ -43,6 +43,59 @@ void get_boundary_edges(
 	igl::boundary_facets(F, out_b_edge);
 }
 
+double area_of_triangle(
+	Eigen::MatrixXd const & vertex_1,
+	Eigen::MatrixXd const & vertex_2,
+	Eigen::MatrixXd const & vertex_3
+	)
+{
+	// vertex_i : 1-by-3 matrix, a row of the vertices matrix
+	//
+	// return : double, area of triangle with vertices at v1,2,3
+	double side[3];
+	double s;
+	side[0] = (vertex_2 - vertex_1).norm();
+	side[1] = (vertex_3 - vertex_2).norm();
+	side[2] = (vertex_1 - vertex_3).norm();
+	s = 0.5*(side[0] + side[1] + side[2]);
+	return sqrt(s*(s-side[0])*(s-side[1])*(s-side[2]));
+}
+
+void compute_area(
+	Eigen::MatrixXd const & V,
+	Eigen::MatrixXi const & F,
+	Eigen::SparseMatrix<double> & Area
+	)
+{
+	// V : input vertices, N-by-3
+	// F : input faces
+	//
+	// Area : output diagonal matrix, N-by-N, each entry represents the normalising area around a vertex
+	int faces = F.rows();
+	int vertices = V.rows();
+	double area_tri[faces];
+	std::vector<std::vector<int>> neighbourFacesOf(vertices);
+	for (int i = 0; i < faces; ++i)
+	{
+		neighbourFacesOf[F(i,0)].push_back(i);
+		neighbourFacesOf[F(i,1)].push_back(i);
+		neighbourFacesOf[F(i,2)].push_back(i);
+		area_tri[i] = area_of_triangle(V.row(F(i,0)), V.row(F(i,1)), V.row(F(i,2)))/3;
+	}
+
+	Area.resize(vertices, vertices);
+	Area.setZero();
+	for (int i = 0; i < vertices; ++i)
+	{
+		double area = 0;
+		for (int index:neighbourFacesOf[i])
+		{
+			area += area_tri[index];
+		}
+		Area.insert(i,i) = area;
+	}
+}
+
 void compute_H(
 	Eigen::MatrixXd const & V,
 	Eigen::MatrixXi const & F,
@@ -66,15 +119,25 @@ void compute_H(
 
 	Eigen::SparseMatrix<double> L, Area, AreaInv;
 	igl::cotmatrix(V,F,L);
-	igl::massmatrix(V,F, igl::MASSMATRIX_TYPE_VORONOI, Area);
+	//igl::massmatrix(V,F, igl::MASSMATRIX_TYPE_VORONOI, Area);
+	compute_area(V, F, Area);
 
+	AreaInv.resize(Area.rows(), Area.rows());
+	AreaInv.setZero();
+	for (int i = 0; i < Area.rows(); ++i)
+	{
+		AreaInv.insert(i,i) = 1/Area.coeff(i,i);
+	}
+
+	Eigen::MatrixXd LBV = AreaInv*L*V;
 	//------------------------------------------
 	// replace this 
-	H.resize(V.rows());
-	H.setZero();
+	H.resize(LBV.rows());
 	
-    
-
+	for (int i = 0; i < LBV.rows(); ++i)
+	{
+		H(i) = 0.5*LBV.row(i).norm();
+	}
 }
 
 
--- tutorial_004/cpp/practices_laplacian/src/mytools.h
+++ tutorial_004/cpp/practices_laplacian/src/mytools.h
@@ -22,6 +22,17 @@
 void get_boundary_vex(Eigen::MatrixXd const & V_in, Eigen::MatrixXi const & F_in, Eigen::MatrixXd & out_bvex);
 void get_boundary_edges(Eigen::MatrixXi const & F_in, Eigen::MatrixXi & out_bedge);
 
+double area_of_triangle(
+	Eigen::MatrixXd const & vertex_1,
+	Eigen::MatrixXd const & vertex_2,
+	Eigen::MatrixXd const & vertex_3);
+
+
+void compute_area(
+	Eigen::MatrixXd const & V,
+	Eigen::MatrixXi const & F,
+	Eigen::SparseMatrix<double> & Area);
+
 void compute_H(Eigen::MatrixXd const & V, Eigen::MatrixXi const & F, Eigen::VectorXd & H);
 
 void calculate_vertex_normal(
