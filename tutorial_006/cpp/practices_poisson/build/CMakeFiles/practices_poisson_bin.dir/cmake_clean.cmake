579c1333fddcdb856e6ac4731a84ad8340e86a6d
Shenggang Hu <hushenggang1996@163.com>
2019-03-16 20:38:31 +0000

    prework commit

---
+++ tutorial_006/cpp/practices_poisson/build/CMakeFiles/practices_poisson_bin.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practices_poisson_bin.dir/main.cpp.o"
+  "CMakeFiles/practices_poisson_bin.dir/mytools.cpp.o"
+  "practices_poisson_bin.pdb"
+  "practices_poisson_bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practices_poisson_bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
